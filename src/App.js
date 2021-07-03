ab11e4b95c69654291c9b8ff3e6d2bbabed36038
KOJI-YAMABE <kvbl.ak25@gmail.com>
2021-07-03 15:39:41 +0900

    lrean jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React from "react";
 
 function App() {
-  return<div><h1>Hello World</h1></div>;
+  return (
+  <React.Fragment>
+    <label htmlFor="bar">bar</label>
+     <input type="text" onChange={() => {console.log("I am Koji.")}} />
+  </React.Fragment>
+  )
 }
+
+
+// function App() { 
+//   return React.createElement(
+//     "h1",
+//     null,
+//     "Hello world!!"
+//   );
+// }
 
 export default App;
